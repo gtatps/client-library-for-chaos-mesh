d0a8e393a91ab6dead94b6deaacfa413673dcec2
Gash Teshome <gash@moveparallel.com>
2025-03-28 17:44:38 -0700

    Add test for network delay and update README
    Also clean up network delay example

--- examples/network-delay.py
+++ examples/network-delay.py
@@ -1,27 +1,30 @@
-import time 
+import time
 
 from chaosmesh.client import Client, Experiment
 from chaosmesh.k8s.selector import Selector
+
+# namespace in which to create the NetworkChaos object
+ns_name = "default"
 
 # creating the ChaosMesh client
 client = Client(version="v1alpha1")
 
 # target pods selector; by labelSector or by pods in specified namespaces
-selector = Selector(labelSelectors={"app": "webrtc-server"}, pods=None, namespaces=['ps-dev'])
+selector = Selector(labelSelectors={"app": "echoserver"}, pods=None, namespaces=[ns_name])
 
 # name of the experiment
 exp_name = "network-delay-100ms"
 
-# namespace in which to create the NetworkChaos object
-ns_name = "ps-dev"
-
 # default latency
 latency = "100ms"
+
+# time in seconds to sleep before deleting experiment
+sleep_time = 10
 
 # starting up the pod failure experiment
 client.start_experiment(Experiment.NETWORK_DELAY, namespace=ns_name, name=exp_name, selector=selector, latency=latency)
 
 # After we're done with the experiment we should delete it
-time.sleep(10)
+time.sleep(sleep_time)
 client.delete_experiment(Experiment.NETWORK_DELAY, namespace=ns_name, name=exp_name)
 
